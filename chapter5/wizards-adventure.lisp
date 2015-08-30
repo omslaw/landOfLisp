d195edd68a3440453b129ea3fff13e23da08917c
omslaw <cafeconleche@overhere.com>
2015-08-29 22:08:10 -0700

    Chapter 5: Wizards Adventure Text Game
    Added the game edges

--- chapter5/wizards-adventure.lisp
+++ chapter5/wizards-adventure.lisp
@@ -6,3 +6,9 @@
 				     there is a well in front of you.))
 			(attic (you are in the attic.
 				    there is a giant welding torch in the corner.))))
+
+(defparameter *edges* '((living-room (garden west door)
+				     (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
