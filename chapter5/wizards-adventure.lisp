b77e4958a15bacf9ca3b86c3025ef288ba5ec033
omslaw <cafeconleche@overhere.com>
2015-08-29 21:53:49 -0700

    Initial progress chapter 5
    Creating Wizards Adventure

---
+++ chapter5/wizards-adventure.lisp
@@ -0,0 +1,8 @@
+;; Chapter 5 Wizards Adventure Text Game
+
+(defparameter *nodes* '((living-room (you are in the living room.
+					  a wizard is snoring loudly on the couch.))
+			(garden (you in a beautiful garden.
+				     there is a well in front of you.))
+			(attic (you are in the attic.
+				    there is a giant welding torch in the corner.))))
